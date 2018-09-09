55a7d3ea7a65a67f90360db31c0247c0a3c9f662
Matthew Jesuele <matt@mvj.fyi>
2018-09-08 17:57:57 -0700

    Add ContentContainer and presentational Content component

---
+++ src/ContentContainer.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { Entry } from "./hardcodedData";
+import { EntryRepo, IEntryRepo } from "./repositories/entryRepo";
+
+type ContentContainerProps = { slug: string, entryRepo?: IEntryRepo };
+type ContentContainerState = { entry?: Entry, entryRepo: IEntryRepo };
+
+export class ContentContainer extends React.Component<ContentContainerProps, ContentContainerState> {
+  public constructor(props: ContentContainerProps) {
+    super(props);
+    this.state = {
+      entryRepo: props.entryRepo || new EntryRepo(),
+    };
+  }
+
+  public componentDidMount() {
+    this.setState(state => ({
+      ...state,
+      entry: this.state.entryRepo.getEntryBySlug(this.props.slug),
+    }));
+  }
+
+  public render() {
+    if (!this.state.entry) {
+      return <p>No entry found at that URL</p>;
+    }
+
+    return <Content entry={this.state.entry} />;
+  }
+}
+
+export const Content: React.SFC<{ entry: Entry }> = ({ entry }) => {
+  const { images, text } = entry.content;
+  return (
+    <>
+      <h1>{entry.title}</h1>
+      {text && <p>{text}</p>}
+      {images && images.map(img => <img key={img} src={img} />)}
+    </>
+  );
+};
