5f412607036da44337faceffc29ea1f4c8964718
Matthew Jesuele <matt@mvj.fyi>
2018-09-10 12:44:40 -0700

    EntryRepo can add entries

--- src/repositories/entryRepo.test.ts
+++ src/repositories/entryRepo.test.ts
@@ -4,28 +4,36 @@ import { EntryRepo } from "./entryRepo";
 function subject(dataAccess: any) {
   return new EntryRepo(dataAccess);
 }
+describe("EntryRepo", () => {
+  it("finds an entry by slug", () => {
+    const repo = subject(testDataAccess());
 
-it("finds an entry by slug", () => {
-  const repo = subject(testDataAccess());
+    expect(repo.getEntryBySlug("birth")).toHaveProperty("slug", "birth");
+    expect(repo.getEntryBySlug("death")).toBe(undefined);
+  });
 
-  expect(repo.getEntryBySlug("birth")).toHaveProperty("slug", "birth");
-  expect(repo.getEntryBySlug("death")).toBe(undefined);
-});
+  it("adds an entry", () => {
+    const repo = subject(testDataAccess());
+    repo.addEntry("foo", new Date());
 
-function testDataAccess(props?: Partial<Data>): Data {
-  return {
-    name: "Foouser",
-    entries: [
-      {
-        title: "Birth",
-        date: new Date("1988-12-28"),
-        slug: "birth",
-        content: {
-          text: "Matt was born",
-          images: ["https://loremflickr.com/320/240"],
+    expect(repo.getEntryBySlug("foo")).toHaveProperty("title", "foo");
+  });
+
+  function testDataAccess(props?: Partial<Data>): Data {
+    return {
+      name: "Foouser",
+      entries: [
+        {
+          title: "Birth",
+          date: new Date("1988-12-28"),
+          slug: "birth",
+          content: {
+            text: "Matt was born",
+            images: ["https://loremflickr.com/320/240"],
+          },
         },
-      },
-    ],
-    ...props,
-  };
-}
+      ],
+      ...props,
+    };
+  }
+});
--- src/repositories/entryRepo.ts
+++ src/repositories/entryRepo.ts
@@ -1,6 +1,8 @@
+import slugify from "slugify";
 import { Entry, hardcodedData } from "../hardcodedData";
 
 export interface IEntryRepo {
+  addEntry(title: string, date: Date): boolean;
   getEntryBySlug(slug: string): Entry | undefined;
 }
 
@@ -8,6 +10,25 @@ export class EntryRepo implements IEntryRepo {
   public constructor(
     private dataAccess = hardcodedData,
   ) {}
+
+  public addEntry(title: string, date: Date) {
+    const slug = slugify(title);
+    try {
+      this.dataAccess.entries = [
+        ...this.dataAccess.entries,
+        {
+          title,
+          date,
+          slug,
+          content: {},
+        },
+      ];
+    } catch (e) {
+      return false;
+    }
+    
+    return true;
+  }
 
   public getEntryBySlug(slug: string) {
     return this.dataAccess.entries.find(e => e.slug === slug);
